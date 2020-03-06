bad3b96bc2bc20b647c640dd3d3751f200cfb7ba
oky dwi hartanto <oky.hartanto@m2.formulatrix.com>
2020-03-06 14:21:33 +0700

    async

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
    entry: './src/main.js',
    output: {
-      path: path.join(__dirname, '/bundle'),
+      path: path.join(__dirname, '/www'),
       filename: 'index_bundle.js'
    },
    devServer: {
